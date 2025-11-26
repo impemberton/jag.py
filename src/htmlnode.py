f6daf878dbfaa360f2e7abad9429cfd60b774f63
iketyke <isaac.mellad.pemberton@gmail.com>
2025-11-26 15:48:46 +0000

    write and test htmlnode

---
+++ src/htmlnode.py
@@ -0,0 +1,21 @@
+
+class HTMLNode:
+    def __init__(self, tag=None, value=None, children=None, props=None) -> None:
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError
+
+    def props_to_html(self):
+        props_string = ""
+        if self.props is not None:
+            for prop in self.props:
+                props_string += f' {prop}="{self.props[prop]}"'
+        return props_string
+
+    def __repr__(self) -> str:
+        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+        
---
+++ src/test_htmlnode.py
@@ -0,0 +1,42 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+
+class TestHTMLNode(unittest.TestCase):
+    def test_tag(self):
+        tag = "p"
+        node = HTMLNode(tag, "this is some text")
+        self.assertEqual(node.tag, tag)
+
+    def test_value(self):
+        tag = "p"
+        value = "This is a value"
+        node = HTMLNode(tag, value)
+        self.assertEqual(node.value, value)
+
+    def test_children(self):
+        tag = "p"
+        value = "This is a value"
+        children = [HTMLNode("p"), HTMLNode("h1")]
+        node = HTMLNode(tag, value, children)
+        self.assertEqual(node.children, children)
+
+    def test_props(self):
+        tag = "a"
+        value = "This is a value"
+        children = [HTMLNode("p"), HTMLNode("h1")]
+        props = {"href": "https://www.test.com"}
+        node = HTMLNode(tag, value, children, props)
+        self.assertEqual(node.props, props)
+
+    def test_to_html(self):
+        tag = "p"
+        node = HTMLNode(tag, "this is some text")
+        self.assertRaises(NotImplementedError, node.to_html)
+
+    def test_props_to_html(self):
+        props = {"href": "https://www.test.com", "target": "_blank"}
+        test_string = ' href="https://www.test.com" target="_blank"'
+        node = HTMLNode(props=props)
+        self.assertEqual(node.props_to_html(), test_string)
